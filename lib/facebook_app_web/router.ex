bc7ba575d9a1e643c15943fc4f82813d3ca68f98
dyeyuen <dyeyuenofficial@gmail.com>
2018-02-28 13:49:37 +0800

    used resources instead of custom routes

--- lib/facebook_app_web/router.ex
+++ lib/facebook_app_web/router.ex
@@ -20,20 +20,16 @@ defmodule FacebookAppWeb.Router do
 
   scope "/api", FacebookAppWeb do
     pipe_through [:api, :authentication]
+    resources("/posts", PostController, except: [:new, :edit]) do
+      resources("/comments", CommentController, except: [:new, :edit])
+    end
 
     post "/profile", ProfileController, :create_profile
-    post "/post", PostController, :create_post
-    post "/:post_id/comment", CommentController, :create_comment
     post "/:post_id/react", ReactionController, :react
 
     get "/my/profile", ProfileController, :get_user_profile
-    get "/my/posts", PostController, :get_user_posts
-    get "/:post_id/comments", CommentController, :get_post_comments
-    get "/:post_id/reactions", ReactionController, :get_post_reactions
-
     get "/logout", UserController, :logout
 
-    get "/posts", PostController, :index
     get "/users", UserController, :index
   end
 
