925ebd9546aadc6910a5bb9c98ad503d406d4051
dyeyuen <dyeyuenofficial@gmail.com>
2018-02-28 16:28:18 +0800

    render user and profile

--- lib/facebook_app_web/views/comment_view.ex
+++ lib/facebook_app_web/views/comment_view.ex
@@ -1,6 +1,7 @@
 defmodule FacebookAppWeb.CommentView do
   use FacebookAppWeb, :view
   alias FacebookAppWeb.CommentView
+  alias FacebookAppWeb.UserView
 
   def render("index.json", %{comments: comments}) do
     %{data: render_many(comments, CommentView, "comment.json")}
@@ -12,7 +13,8 @@ defmodule FacebookAppWeb.CommentView do
 
   def render("comment.json", %{comment: comment}) do
     %{id: comment.id,
-      content: comment.content}
+      content: comment.content,
+      user: render_one(comment.user, UserView, "user.json")}
   end
 
   def render("post_comment.json", %{comments: comments}) do
